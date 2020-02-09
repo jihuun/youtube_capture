b71c484a18314f77ce278738607df6b52de488ef
Ji-Hun Kim <jihuun.k@gmail.com>
2020-02-09 18:42:35 +0900

    refactor: implement youtube download
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

---
+++ make_cap_data.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# Generate default video information json structure
+
+from copy import deepcopy
+
+video_info = {
+        'url': '',
+        'title': '',
+        'video_id': '',
+        'lang_code': 'en',
+        'font_size': 30,
+        'file_name': '',
+        'file_path': '',
+        'nosub_opt': False,
+        'imgdiff_opt': False,
+        'bg_opacity': 0.7,
+        'thumbnail': '',
+        'frame_infos': []
+        }
+
+class cap_data(dict):
+    def __init__(self):
+        self.update(video_info)
+
+def example():
+    print(cap_data())
+
+if __name__ == '__main__':
+    example()

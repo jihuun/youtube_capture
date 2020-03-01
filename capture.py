6f8090d53d62d918200170c499e70ec17e1b8599
Ji-Hun Kim <jihuun.k@gmail.com>
2020-03-01 21:31:58 +0900

    refactor: capture imgs by frame info in json
    - implement capturing each frames from video in capture.py
      And save it in the imgs/ directory
    - implement bake_caption in subtitle.py
    - small fix for passing argument method
    
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- capture.py
+++ capture.py
@@ -1,4 +1,84 @@
 #! /usr/bin/env python3
+import os
+import cv2			#pip3 install opencv-python
+				# If there would be "numpy.core.multiarray" problem of numpy
+				#Do 'pip3 uninstall numpy' few times until all numpy version will be removed.
+				# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
+from subtitle import bake_caption
+
+IMG_FORMAT = '.jpg'
+FONT_FILE = 'NanumGothic.ttf'
+
+class capture_by_subs():
+    def __init__(self, v_info):
+        self.__save_args(v_info)
+        self.__mkdir_imgs()
+        self.__capture_video()
+
+    def __save_args(self, dic):
+        self.img_path = os.path.join(dic['file_path'], 'imgs')
+        self.vid_path = os.path.join(dic['file_path'], dic['file_name'] + '.mp4')
+        self.vid_info = dic
+        self.frm_info = dic['frame_infos']
+
+    def __mkdir_imgs(self):
+        os.makedirs(self.img_path)
+
+    def __capture_video(self):
+        cap = cv2.VideoCapture(self.vid_path)
+        self.__capture(cap)
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def __capture(self, cap):
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        f_total= len(self.frm_info)
+        f_cnt = cap_cnt = 0
+
+        #for idx, f_dic in enumerate(self.frm_info):
+        while(cap_cnt < f_total):
+            ret, frame = cap.read()
+            duration = float(f_cnt) / float(fps)
+
+            if duration > self.frm_info[cap_cnt]['time_info']: # the capture moment
+                self.__save_frame(frame, cap_cnt)
+                self.__bake_caption(cap_cnt)
+                cap_cnt += 1
+
+            f_cnt += 1
+
+    def __save_frame(self, frame, idx):
+        save_path = self.__make_frm_path(idx)
+        self.frm_info[idx]['img_path'] = save_path
+        self.__cv_save_image(frame, save_path)
+
+    def __make_frm_path(self, idx):
+        ret = os.path.join(self.img_path, self.__numbering('frame', idx) + IMG_FORMAT)
+        return ret
+
+    def __numbering(self, name, num):
+        return '%s_%s' %(name, num)
+
+    def __cv_save_image(self, frame, path):
+        '''
+        rate = '%d/%d  %fs' %(__cnt, (__tot_frame - 1), __duration)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 0.8
+        fs = round(__font_size * font_scale)
+        pos = (10, fs + 10)
+        color = (255, 255, 0) #(B,G,R)
+        thickness = 1
+        linetype = cv2.LINE_AA
+        cv2.putText(__frame, rate, pos, font, font_scale, color, thickness, linetype)
+        '''
+        cv2.imwrite(path, frame)
+
+    def __bake_caption(self, idx):
+        path = self.frm_info[idx]['img_path']
+        text = self.frm_info[idx]['script']
+        font_size = self.vid_info['font_size']
+        background_opacity = self.vid_info['bg_opacity']
+        bake_caption(path, text, FONT_FILE, font_size, background_opacity)
 
 def main():
     pass
--- run.py
+++ run.py
@@ -8,66 +8,90 @@ from logger import *
 from youtube import youtube
 from subtitle import srt_to_list
 from make_cap_data import cap_data
+from capture import capture_by_subs
+
+DEF_L_CODE = 'en'
+DEF_VID_NAME = 'dl_video'
 
 class make_youtube_info(dict):
-    def __init__(self, args):
-        self.args = args
-        self.update(cap_data())
-        vid, srt = self.download_youtube()
-        self.save_variables(vid, srt)
-        self.set_frame_info(srt)
+    def __init__(self, url, name=DEF_VID_NAME, lang=DEF_L_CODE, retry=False, fontsize=30):
+        self.__save_args(url, name, lang, retry, fontsize)
+        self.update(cap_data()) # get basic json structure
+        res, vid, srt = self.__download_youtube()
+        self.__save_variables(vid, srt)
+        self.__set_frame_info(srt) # add frame info to the json
+        self.__update_header(res)
 
-    def save_variables(self, vid, srt):
+    def __save_args(self, url, name, lang, retry, fontsize):
+        self.arg_url = url
+        self.arg_name = name
+        self.arg_lang = lang
+        self.arg_retry = retry
+        self.arg_fontsize = fontsize
+
+    def __download_youtube(self):
+        self.yt = youtube(self.arg_url)
+        res_dir = self.__make_download_dir()
+        vid_path = self.yt.download_video(res_dir, fname=self.arg_name)
+        srt_path = self.yt.download_caption(res_dir, fname=self.arg_name, lang=self.arg_lang)
+        return res_dir, vid_path, srt_path
+
+    def __make_download_dir(self):
+        result_path = os.path.join(get_curpath(), 'results', self.arg_name)
+        if self.arg_retry and os.path.exists(result_path):
+            shutil.rmtree(result_path)
+        os.makedirs(result_path)
+        return result_path
+
+    def __save_variables(self, vid, srt):
         self.vid_path = vid
         self.srt_path = srt
         self.json_path = srt.replace('.srt', '.json')
 
-    def download_youtube(self):
-        yt = youtube(self.args.url)
-        res_dir = self.make_download_dir()
-        vid_path = yt.download_video(res_dir, fname=self.args.name)
-        srt_path = yt.download_caption(res_dir, fname=self.args.name, lang=self.args.lang)
-        return vid_path, srt_path
-
-    def set_frame_info(self, srtfile_path):
+    def __set_frame_info(self, srtfile_path):
         self['frame_infos'] = srt_to_list(srtfile_path)
+
+    def __update_header(self, ret_path):
+        self['url'] = self.arg_url
+        self['title'] = self.yt.info.title
+        self['video_id'] = self.yt.info.video_id
+        self['lang_code'] = self.arg_lang
+        self['font_size'] = self.arg_fontsize
+        self['file_name'] = self.arg_name
+        self['file_path'] = ret_path
+        self['nosub_opt'] = None
+        self['imgdiff_opt'] = None
+        self['bg_opacity'] = 0.7 # TODO: get form option?
+        self['thumbnail'] = 'https://img.youtube.com/vi/%s/maxresdefault.jpg' %self.yt.info.video_id
 
     def save_json(self):
         with open(self.json_path, 'w') as fp:
             v_infos_json = json.dumps(self, ensure_ascii=False, indent="\t")
             fp.write(v_infos_json)
 
-    def make_download_dir(self):
-        result_path = os.path.join(get_curpath(), 'results', self.args.name)
-        if self.args.retry and os.path.exists(result_path):
-            shutil.rmtree(result_path)
-        os.makedirs(result_path)
-        return result_path
-
 def get_curpath():
     return os.getcwd()
 
-DEFAULT_L_CODE = 'en'
-DEFAULT_VID_NAME = 'dl_video'
 def parse_args():
-	parser = argparse.ArgumentParser(description='Screen capture automatically from Youtube video\nexample: run.py -u <youtube link> -n <outfile name> -l <language> -f <fontsize>')
-	parser.add_argument('-u', '--url', dest='url', help='Youtube vedio url')
-	parser.add_argument('-n', '--name', dest='name', default=DEFAULT_VID_NAME, help='Output file name')
-	parser.add_argument('-l', '--lang', dest='lang', default=DEFAULT_L_CODE, help='Caption language code (default: en)')
-	parser.add_argument('-f', '--fontsize', dest='fontsize', default=30, type=int, help='Font size of caption (default: 30)')
-	parser.add_argument('-b', '--bg-opacity', dest='bg_opacity', default=0, type=float, help='Add backgound behind of caption text with opacity (0.0 ~ 1.0) (default opacity : 0.0)')
-	parser.add_argument('--no-sub', dest='nosub', action='store_true', help='If the video has a closed caption, no need to add caption additionally')
-	parser.add_argument('--img-diff', dest='imgdiff', action='store_true', help='capture frame by imagediff')
-	parser.add_argument('-r', '--retry', dest='retry', action='store_true', help='Retry download video')
+    parser = argparse.ArgumentParser(description='Screen capture automatically from Youtube video\nexample: run.py -u <youtube link> -n <outfile name> -l <language> -f <fontsize>')
+    parser.add_argument('-u', '--url', dest='url', help='Youtube vedio url')
+    parser.add_argument('-n', '--name', dest='name', default=DEF_VID_NAME, help='Output file name')
+    parser.add_argument('-l', '--lang', dest='lang', default=DEF_L_CODE, help='Caption language code (default: en)')
+    parser.add_argument('-f', '--fontsize', dest='fontsize', default=30, type=int, help='Font size of caption (default: 30)')
+    parser.add_argument('-b', '--bg-opacity', dest='bg_opacity', default=0, type=float, help='Add backgound behind of caption text with opacity (0.0 ~ 1.0) (default opacity : 0.0)')
+    parser.add_argument('--no-sub', dest='nosub', action='store_true', help='If the video has a closed caption, no need to add caption additionally')
+    parser.add_argument('--img-diff', dest='imgdiff', action='store_true', help='capture frame by imagediff')
+    parser.add_argument('-r', '--retry', dest='retry', action='store_true', help='Retry download video')
 
-	args = parser.parse_args()
-	logger.info(args)
-	return args
+    args = parser.parse_args()
+    logger.info(args)
+    return args
 
 def main():
     args = parse_args()
-    video_info = make_youtube_info(args)
+    video_info = make_youtube_info(args.url, args.name, args.lang, args.retry, args.fontsize)
     video_info.save_json()
+    capture_by_subs(video_info)
     '''
     video, caption, v_infos = download_youtube(args)
     if need_modify_cap():
